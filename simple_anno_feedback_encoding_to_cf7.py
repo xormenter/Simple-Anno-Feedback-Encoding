588a85f6eb13ba72008e625184c908e7711efbc3
xormenter <94999291+xormenter@users.noreply.github.com>
2021-12-18 18:48:28 +0100

    Update simple_anno_feedback_encoding_to_cf7.py

--- simple_anno_feedback_encoding_to_cf7.py
+++ simple_anno_feedback_encoding_to_cf7.py
@@ -4,7 +4,30 @@ from pathlib import Path
 
 import argparse
 from io import StringIO
+import difflib
 
+
+SEQUENCE_ID_BY_NAME = {"idle01":1000, "idle02":1001, "idle03":1002, "idle04":1003, "idle05":1003, "death01":1005, "talk01":1010, "talk02":1011, "greet01":1020, \
+    "bow01":1021, "cheer01":1030, "cheer02":1031, "cheer03":1032, "lookat01":1040, "lookat02":1041, "protest01":1050, "protest02":1051, "laydown01":1060, \
+    "laydown02":1061, "laydown03":1062, "fishing01":1070, "fishing02":1071, "fishing03":1072, "dance01":1080, "dance02":1081, "dance03":1082, "dance04":1083, "fight01":1090, \
+    "fight02":1091, "walk01":2000, "walk02":2001, "walk03":2002, "walk04":2003, "walk05":2004, "walk06":2005, "walk07":2005, "drunkenwalk01":2010, "drunkenwalk02":2011, \
+    "run01":2100, "panicrun01":2101, "panicrun02":2102, "donate01":2200, "buy01":2201, "buy02":2202, "work01":3000, "work02":3001, "work03":3002, \
+    "work04":3003, "work05":3004, "work06":3005, "stand01":4000, "build01":5000, "portrait_neutral_idle":10000, "portrait_neutral_talk":10001, \
+    "portrait_friendly_idle":10010, "portrait_friendly_talk":10011, "portrait_angry_idle":10020, "portrait_angry_talk":10021, "portrait_neutral_talk_idle":10030, \
+    "portrait_friendly_talk_idle":10031, "portrait_angry_talk_idle":10040, "extFire01":5100, "extFire02":5101, "extFire03":5102, "pray01":5200, \
+    "protestwalk01":5300, "protestwalk02":5301, "protest03":1052, "protest04":1053, "protest05":1054, "protest06":1055, "fight03":1092, "protestwalk03":5302, \
+    "fight04":1093, "fight05":1094, "work_staged01":3010, "work_staged02":3011, "work_staged03":3012, "takeoff01":5400, "land01":5410, "riotspecial01":5350, \
+    "riotspecial02":5351, "boosted":3050, "riotspecial03":5352, "sitdown01":5500, "sitdown02":5501, "sitdown03":5502, "explode01":2300, "explode02":2301, \
+    "explode03":2302, "explode04":2303, "idleLoaded01":6000, "walkingLoaded01":6001, "hitwood":2400, "hitbrick":2401, "hitsteel":2402, "hitconcrete":2403, \
+    "misswater":2410, "missland":2411, "work07":3006, "work08":3007, "work09":3008, "work10":3009, "work11":3020, "work12":3021, "work13":3022, \
+    "work14":3023, "work15":3024, "work16":3025, "work17":3026, "work18":3027, "work19":3028}
+
+def get_sequence(sequence):
+    if sequence not in SEQUENCE_ID_BY_NAME:
+        closest = difflib.get_close_matches(sequence, SEQUENCE_ID_BY_NAME.keys())
+        print(f"Unknown sequence {sequence}. Did you mean one of {closest}? Use --sequence_codes to get a list of valid sequences.")
+        exit(1)
+    return str(SEQUENCE_ID_BY_NAME[sequence])
 
 def get_text(node, query, default = ""):
     if node.find(query) is not None:
@@ -49,6 +72,8 @@ class FeedbackConfig():
         self.min_scale = get_required_text(scale_node, "m_MinScaleFactor")
         self.max_scale = get_required_text(scale_node, "m_MaxScaleFactor")
 
+
+
     def extract_sequence(self):
         self.sequence_elements = []
         for sequence_element_node in list(self.node.find("SequenceElements")):
@@ -56,7 +81,7 @@ class FeedbackConfig():
             etree.SubElement(element, "hasValue").text = "1"
             if sequence_element_node.tag == "IdleAnimation":
                 etree.SubElement(element, "elementType").text = "1"
-                etree.SubElement(element, "m_IdleSequenceID").text = get_required_text(sequence_element_node, "m_IdleSequenceID")
+                etree.SubElement(element, "m_IdleSequenceID").text = get_sequence(get_required_text(sequence_element_node, "m_IdleSequenceID"))
                 etree.SubElement(element, "MinPlayCount").text = get_required_text(sequence_element_node, "MinPlayCount")
                 etree.SubElement(element, "MaxPlayCount").text = get_required_text(sequence_element_node, "MaxPlayCount")
                 etree.SubElement(element, "MinPlayTime").text = "0"
@@ -64,7 +89,7 @@ class FeedbackConfig():
                 etree.SubElement(element, "ResetStartTime").text = "0"
             if sequence_element_node.tag == "TimedIdleAnimation":
                 etree.SubElement(element, "elementType").text = "1"
-                etree.SubElement(element, "m_IdleSequenceID").text = get_required_text(sequence_element_node, "m_IdleSequenceID")
+                etree.SubElement(element, "m_IdleSequenceID").text = get_sequence(get_required_text(sequence_element_node, "m_IdleSequenceID"))
                 etree.SubElement(element, "MinPlayCount").text = "0"
                 etree.SubElement(element, "MaxPlayCount").text = "0"
                 etree.SubElement(element, "MinPlayTime").text = get_required_text(sequence_element_node, "MinPlayTime")
@@ -72,7 +97,7 @@ class FeedbackConfig():
                 etree.SubElement(element, "ResetStartTime").text = "0"
             if sequence_element_node.tag == "Walk":
                 etree.SubElement(element, "elementType").text = "0"
-                etree.SubElement(element, "WalkSequence").text = get_required_text(sequence_element_node, "WalkSequence")
+                etree.SubElement(element, "WalkSequence").text = get_sequence(get_required_text(sequence_element_node, "WalkSequence"))
                 etree.SubElement(element, "TargetDummy").text = get_required_text(sequence_element_node, "TargetDummy")
                 etree.SubElement(element, "TargetDummyId").text = self.feedback_encoding.dummy_id_by_name[get_required_text(sequence_element_node, "TargetDummy")]
                 etree.SubElement(element, "SpeedFactorF").text = get_required_text(sequence_element_node, "SpeedFactorF")
@@ -242,9 +267,13 @@ if __name__ == "__main__":
         
     parser = argparse.ArgumentParser("safe_to_cf7")
     parser.add_argument("-i", "--input", dest="filename", required=True, type=str,
-                            help="input file (.safe.xml)", metavar="FILE")
+                            help="input file (.xml)", metavar="FILE")
+    parser.add_argument("-s", "--sequence_codes", help="Print list of valid sequence codes", action = "store_true")
     args = parser.parse_args()
-    print(args.filename)
+    if (args.sequence_codes):
+        print("Valid sequence codes: ")
+        for sequence_name in SEQUENCE_ID_BY_NAME:
+            print(sequence_name)
 
     tree = etree.parse(args.filename)
     root = tree.getroot()
